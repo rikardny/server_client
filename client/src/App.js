6157d27655f32eb737b666bdfb2dde5343c9b85a
Jonathan Alvarsson <jonathan.alvarsson@gmail.com>
2018-07-10 09:55:51 +0200

    spelling

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Header }from './Header';
+import { Header } from './Header';
 import { MenuBox } from './MenuBox';
 import { ResultList } from './ResultList';
 import 'bulma/css/bulma.css';
